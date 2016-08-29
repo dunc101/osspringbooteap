c26f83d9210439562af7ad719ac7787e03d6f2aa
dunc101 <dunc101@gmail.com>
2016-08-29 16:48:25 -0500

    Removing ELM due to OS not being able to pull dependen cies for this example

--- src/main/java/com/ipacc/enterprise/elm/examples/OSSpringBootEAPExample.java
+++ src/main/java/com/ipacc/enterprise/elm/examples/OSSpringBootEAPExample.java
@@ -1,6 +1,5 @@
 package com.ipacc.enterprise.elm.examples;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
@@ -11,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,17 +17,10 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.servlet.DispatcherServlet;
 
 import com.ipacc.enterprise.elm.examples.config.ExampleConfig;
-//import com.ipacc.enterprise.elm.logging.AbstractInfinityMetric;
-import com.ipacc.enterprise.elm.logging.HealthCheck;
-import com.ipacc.enterprise.elm.logging.InfinityLogger;
-import com.ipacc.enterprise.elm.logging.InfinityLoggerFactory;
-
 @SpringBootApplication
 @Component
-@ComponentScan({"com.ipacc.enterprise.elm.examples.config"})
 @RestController
-public class ELMSpringBootJbossTemplate extends SpringBootServletInitializer {
-	private static InfinityLogger logger = InfinityLoggerFactory.getInstance(ELMSpringBootJbossTemplate.class);
+public class OSSpringBootEAPExample extends SpringBootServletInitializer {
 	@Autowired
 	ExampleConfig exampleConfig;
 	//@Autowired
@@ -39,29 +30,6 @@ public class ELMSpringBootJbossTemplate extends SpringBootServletInitializer {
 	public String ping() {
 		return "pong";
 	}
-	
-	@PostConstruct
-	public void addHealthChecks() {
-		logger.debug("Adding health checks.");
-		try {
-			// ping google once a minute and determine if it is up ... 
-			HealthCheck healthCheck = new HealthCheck(logger);
-			healthCheck.pingExternalURL("Google", exampleConfig.getUrl(), 10000);
-			//health.monitorDatabase("myDataBase", dataSource);
-			// Or create a custom metric
-
-			/*healthCheck.addMetric(new AbstractInfinityMetric("myCustomMetric") {
-			    @Override
-			    public MetricStatus evaluate() {
-			    	//MetricStatus.Red
-			    	//MetricStatus.Yellow
-			        return MetricStatus.Green;
-			    }
-			});*/
-		} catch (Exception e) {
-			logger.error(e.getMessage());
-		}
-	}
 
 	
 	/**
@@ -70,7 +38,7 @@ public class ELMSpringBootJbossTemplate extends SpringBootServletInitializer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		SpringApplication.run(ELMSpringBootJbossTemplate.class, args);
+		SpringApplication.run(OSSpringBootEAPExample.class, args);
 	}
 	
 	/*
@@ -79,7 +47,7 @@ public class ELMSpringBootJbossTemplate extends SpringBootServletInitializer {
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		try (AnnotationConfigWebApplicationContext context  = new AnnotationConfigWebApplicationContext()) {
-			context.register(ELMSpringBootJbossTemplate.class);
+			context.register(OSSpringBootEAPExample.class);
 			context.setServletContext(servletContext);
 			
 			Dynamic registration = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
@@ -93,6 +61,6 @@ public class ELMSpringBootJbossTemplate extends SpringBootServletInitializer {
 
 	 @Override
 	 protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
-		 return application.sources(ELMSpringBootJbossTemplate.class);
+		 return application.sources(OSSpringBootEAPExample.class);
 	 }
 }
